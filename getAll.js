09388a7d00e1c5ebe2da646d42e2b87b180c92a5
Viyas <viyas2001@gmail.com>
2022-10-26 14:37:32 +0530

    MySql : This file list of all connection is used to working in our projects

--- getAll.js
+++
@@ -1,9 +0,0 @@
-exports.getAll = (connection, options) => {
-    return new Promise((resolve, reject) => {
-      connection.query(`SELECT ${options.projection} FROM ${options.table_names}`, (err, results) => {
-        if (err) return reject(err);
-  
-        return resolve(results);
-      });
-    });
-  };
--- insertOne.js
+++
@@ -1,9 +0,0 @@
-exports.insertOne = (connection, options) => {
-    // console.log(options);
-    return new Promise((resolve, reject) => {
-      connection.query(`INSERT INTO ${options.table_name} SET ?`, options.data, (err, results) => {
-        if (err) return reject(err);
-        return resolve(results);
-      });
-    });
-  };
